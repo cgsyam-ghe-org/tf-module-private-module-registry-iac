0d131592a986e8beadb8a5948a467b4b700a5fbd
SWETA_RAJ <swetaraj@google.com>
2024-07-01 12:13:15 +0530

    updated code

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ resource "tfe_registry_module" "module_registry" {
   name         = each.key
   organization = var.tfe_org
   vcs_repo {
-    display_identifier         = each.value.repo_display_identifier
+    display_identifier         = each.value.repo_identifier
     identifier                 = each.value.repo_identifier
     oauth_token_id             = tfe_oauth_client.gh_oauth.oauth_token_id
     branch                     = each.value.repo_branch
